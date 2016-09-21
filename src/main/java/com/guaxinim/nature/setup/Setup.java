83b0c45278c8c3adfcf719a9e324d989a13ec376
Elvis Rocha <elvishrocha@gmail.com>
2016-09-20 23:17:26 -0300

    Fixing Neo4j

--- src/main/java/com/guaxinim/nature/setup/Setup.java
+++ src/main/java/com/guaxinim/nature/setup/Setup.java
@@ -1,17 +1,19 @@
 package com.guaxinim.nature.setup;
 
-import org.neo4j.driver.v1.*;
+import org.neo4j.driver.v1.Driver;
+import org.neo4j.driver.v1.AuthToken;
+import org.neo4j.driver.v1.AuthTokens;
+import org.neo4j.driver.v1.GraphDatabase;
+import org.neo4j.driver.v1.Config;
 
-import javax.ejb.Singleton;
-import javax.ejb.Startup;
+import javax.ejb.Stateless;
 import javax.enterprise.inject.Produces;
 import java.util.logging.Logger;
 
 /**
  * Created by elvis on 16/09/2016.
  */
-@Singleton
-@Startup
+@Stateless
 public class Setup {
 
     Logger logger = Logger.getLogger(this.getClass().getName());
