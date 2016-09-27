fea8d470482a29b4de78e9f0e5ae3dd02a874288
Elvis Rocha <elvishrocha@gmail.com>
2016-09-27 13:55:33 -0300

    Fixing project

--- src/main/java/com/guaxinim/nature/domain/Action.java
+++ src/main/java/com/guaxinim/nature/domain/Action.java
@@ -25,4 +25,51 @@ public class Action extends Entity {
     @Relationship(type = "WITH")
     private Set<Object> smt;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getWhen() {
+        return when;
+    }
+
+    public void setWhen(Date when) {
+        this.when = when;
+    }
+
+    public Set<Person> getDidBy() {
+        return didBy;
+    }
+
+    public void setDidBy(Set<Person> didBy) {
+        this.didBy = didBy;
+    }
+
+    public Set<Person> getToSmb() {
+        return toSmb;
+    }
+
+    public void setToSmb(Set<Person> toSmb) {
+        this.toSmb = toSmb;
+    }
+
+    public Set<Object> getToSmt() {
+        return toSmt;
+    }
+
+    public void setToSmt(Set<Object> toSmt) {
+        this.toSmt = toSmt;
+    }
+
+    public Set<Object> getSmt() {
+        return smt;
+    }
+
+    public void setSmt(Set<Object> smt) {
+        this.smt = smt;
+    }
 }
