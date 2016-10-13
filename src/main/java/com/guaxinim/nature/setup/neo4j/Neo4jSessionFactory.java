a8232e057e1b4cdb236c647a858f2522ae5347fb
Elvis Rocha <elvishrocha@gmail.com>
2016-10-12 23:32:51 -0300

    Trying to solve neo4j driver error

--- src/main/java/com/guaxinim/nature/setup/neo4j/Neo4jSessionFactory.java
+++ src/main/java/com/guaxinim/nature/setup/neo4j/Neo4jSessionFactory.java
@@ -4,8 +4,9 @@ import org.neo4j.ogm.session.Session;
 import org.neo4j.ogm.session.SessionFactory;
 
 public class Neo4jSessionFactory {
-    private final static SessionFactory sessionFactory = new SessionFactory("default.domain");
+
     private static Neo4jSessionFactory factory = new Neo4jSessionFactory();
+    public static final String URL = System.getenv("NEO4J_URL") != null ? System.getenv("NEO4J_URL") : "bolt://localhost";
 
     public static Neo4jSessionFactory getInstance() {
         return factory;
@@ -13,7 +14,17 @@ public class Neo4jSessionFactory {
 
     private Neo4jSessionFactory() { }
 
+    public org.neo4j.ogm.config.Configuration getConfiguration() {
+        org.neo4j.ogm.config.Configuration config = new org.neo4j.ogm.config.Configuration();
+        config
+                .driverConfiguration()
+                .setDriverClassName("org.neo4j.ogm.driver.Driver")
+                .setURI(URL);
+        return config;
+    }
+
     public Session getNeo4jSession() {
+        SessionFactory sessionFactory = new SessionFactory(getConfiguration(), "default.domain");
         return sessionFactory.openSession();
     }
 }
