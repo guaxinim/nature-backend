d26310f70fcd29a63fe99c0e46f0d673574ab37e
Elvis Rocha <elvishrocha@gmail.com>
2016-10-12 23:23:20 -0300

    Adjustments on ActionService, testing with arquillian

--- src/main/java/com/guaxinim/nature/service/ActionService.java
+++ src/main/java/com/guaxinim/nature/service/ActionService.java
@@ -4,20 +4,20 @@ import com.guaxinim.nature.domain.Action;
 import com.guaxinim.nature.persistence.Neo4jGenericService;
 
 import javax.ejb.Stateless;
-import javax.inject.Inject;
 import java.util.Date;
 
 @Stateless
-public class ActionService {
-
-    @Inject
-    Neo4jGenericService<Action> service;
+public class ActionService extends Neo4jGenericService<Action> {
 
     public void saveAction(String strAction) {
         Action action = new Action();
         action.setWhen(new Date());
         action.setName(strAction);
-        service.createOrUpdate(action);
+        createOrUpdate(action);
     }
 
+    @Override
+    public Class<Action> getEntityType() {
+        return Action.class;
+    }
 }
--- src/test/java/com/guaxinim/nature/setup/ActionRestTest.java
+++ src/test/java/com/guaxinim/nature/setup/ActionRestTest.java
@@ -2,6 +2,13 @@ package com.guaxinim.nature.setup;
 
 import com.guaxinim.nature.api.ActionRest;
 import com.guaxinim.nature.domain.Action;
+import com.guaxinim.nature.domain.Entity;
+import com.guaxinim.nature.persistence.Neo4jGenericService;
+import com.guaxinim.nature.persistence.Neo4jService;
+import com.guaxinim.nature.service.ActionService;
+import com.guaxinim.nature.setup.neo4j.Neo4jDriver;
+import com.guaxinim.nature.setup.neo4j.Neo4jSession;
+import com.guaxinim.nature.setup.neo4j.Neo4jSessionFactory;
 import junit.framework.Assert;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.extension.rest.client.ArquillianResteasyResource;
@@ -11,10 +18,12 @@ import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.neo4j.driver.v1.Driver;
+import org.neo4j.driver.v1.Session;
 
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
+import javax.inject.Inject;
 import javax.ws.rs.core.Response;
 import java.io.File;
 import java.util.Date;
@@ -23,12 +32,22 @@ import java.util.List;
 @RunWith(Arquillian.class)
 public class ActionRestTest {
 
-    private Client client = ClientBuilder.newClient();
+    @Inject
+    @Neo4jDriver
+    Driver driver;
+    Session session;
 
     @Deployment
     public static WebArchive createTestArchive() {
         return ShrinkWrap.create(WebArchive.class)
                 .addClass(Setup.class)
+                .addClass(Neo4jDriver.class)
+                .addClass(Neo4jSession.class)
+                .addClass(Neo4jSessionFactory.class)
+                .addPackage("com.guaxinim.nature.domain")
+                .addPackage("com.guaxinim.nature.api")
+                .addPackage("com.guaxinim.nature.persistence")
+                .addPackage("com.guaxinim.nature.service")
                 .addAsWebInfResource(new File("src/main/webapp/WEB-INF/jboss-deployment-structure.xml"))
                 .addAsWebInfResource("wildfly-ds.xml")
                 .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
@@ -36,6 +55,13 @@ public class ActionRestTest {
 
     @Before
     @InSequence(1)
+    public void setupNeo4j() {
+        session = driver.session();
+        Assert.assertTrue(session.isOpen());
+    }
+
+    @Test
+    @InSequence(2)
     public void insertAction(@ArquillianResteasyResource ActionRest actionResource) {
         Action action = new Action();
         action.setName("run");
@@ -44,8 +70,8 @@ public class ActionRestTest {
         Assert.assertTrue(response.getStatus() == 1);
     }
 
-    @Before
-    @InSequence(2)
+    @Test
+    @InSequence(3)
     public void getAction(@ArquillianResteasyResource ActionRest actionResource) {
         List<Action> actions = actionResource.getActions();
         Assert.assertFalse(actions.isEmpty());
